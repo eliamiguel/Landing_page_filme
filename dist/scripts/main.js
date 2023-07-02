9efcdad5915377a31b605ef85ce5acbf56a9e58e
Elias Perreira Miguel <eliasperreiramiguel@gmail.com>
2023-07-02 01:10:24 -0300

    responsividade

---
+++ dist/scripts/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+  $('.lancamentos__list').slick({
+    autoplay: false,
+    dots: false,
+    arrows: true,
+    prevArrow: '<button type="button" class="material-symbols-outlined">arrow_back_ios</button>',
+    nextArrow: '<button type="button" class="material-symbols-outlined">arrow_forward_ios</button>',
+    infinite: false,
+    slidesToShow: 3.5,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2.3
+        }
+      }
+    ]
+    
+  });
+})
