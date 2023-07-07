de25b78b1faad49a8fb08c9319125d9a53eb37f4
Elias Perreira Miguel <eliasperreiramiguel@gmail.com>
2023-07-07 00:57:48 -0300

    melhorando

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -7,9 +7,40 @@ $(document).ready(function() {
     prevArrow: '<button type="button" class="material-symbols-outlined">arrow_back_ios</button>',
     nextArrow: '<button type="button" class="material-symbols-outlined">arrow_forward_ios</button>',
     infinite: false,
-    slidesToShow: 1,
+    slidesToShow: 4,
     slidesToScroll: 1,
-    
-    
+    responsive: [
+      {
+        breakpoint: 400,
+        settings: {
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1.3
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 1082,
+        settings: {
+          slidesToShow: 3.3
+        }
+      },
+      {
+        breakpoint: 1515,
+        settings: {
+          slidesToShow: 4
+        }
+      }
+    ]
   });
+  
 })
